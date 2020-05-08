cd2309699f8878a3bea1251bb0d907af26ab75cb
NessMC <contact@nessmc.fr>
2020-05-08 12:05:16 +0200

    ✨ File reader class added and mocha library added.

--- core/file.js
+++ core/file.js
@@ -2,3 +2,24 @@
          OBJECTIVE HTML
               File
 //////////////////////////////*/
+
+import FS from 'fs'
+
+export default class File {
+
+     constructor (file) {
+          this.file =  file
+     }
+
+     read () {
+
+          return new Promise ((resolve, reject) => {
+               FS.readFile(this.file, 'UTF-8', (error, content) => {
+                    if (error) reject(error)
+                    resolve(content)
+               })
+          })
+
+     }
+
+}
---
+++ tests/File.spec.js
@@ -0,0 +1,1 @@
+import Assert from 'assert'
