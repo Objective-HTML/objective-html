25345fd4cc75d9051e42f08075e69edfd075e8de
NessMC <contact@nessmc.fr>
2020-05-08 12:17:49 +0200

    🧪 File tests added.

--- tests/File.spec.js
+++ tests/File.spec.js
@@ -1,1 +1,24 @@
 import Assert from 'assert'
+import File from '../core/file'
+
+describe('#File', () => {
+
+  it('should return file content', () => {
+    new File('tests/sum.html').read().then(content => {
+      Assert.notEqual(content, undefined)
+    }) 
+  })
+
+  it('should throw an error if file does not exist', () => {
+    new File('tests/index.html').read().catch(error => {
+      Assert.notEqual(error, undefined)
+    })
+  })
+
+  it('should return default value if file is empty', () => {
+    new File('tests/empty.html').read().then(content => {
+      Assert.equal(content, '')
+    })
+  })
+
+})
