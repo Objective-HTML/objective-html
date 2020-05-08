ed58531e1451469d8165113a8134f4779aeddc5b
NessMC <contact@nessmc.fr>
2020-05-08 16:32:45 +0200

    🔨 Conditions minified.

--- core/lexer.js
+++ core/lexer.js
@@ -23,29 +23,23 @@ export default class Lexer {
 
         const inline_code = this.content.split(/\r?\n/g).map(x => x.trim()).filter(x => x !== '').join('')
         const code_status = new Map()
-
         for (const i in inline_code) {
             if (this.status === 'BLOCK_END') this.status = 'BLOCK_VALUE'
             if (this.status === 'BLOCK_START') this.status = 'BLOCK_CONTENT'
-            if (this.status !== 'BLOCK_CONTENT' || this.status !==  'BLOCK_START') {
-                if (inline_code[i] === '<') {
-                    this.status = 'BLOCK_START'
-                }
-            }
-            if (this.status === 'BLOCK_CONTENT') {
-                if (inline_code[i] === '>') {
-                    this.status = 'BLOCK_END'
-                }
-            }
-            if (this.status === 'BLOCK_VALUE') {
-                if (inline_code[i] === '{' || inline_code[i] === '}' ) {
-                    this.status = 'BLOCK_VARIABLE'
-                }
-            }
+            if ((this.status !== 'BLOCK_CONTENT' || this.status !==  'BLOCK_START') && inline_code[i] === '<') this.status = 'BLOCK_START'
+            if (this.status === 'BLOCK_CONTENT' && inline_code[i] === '>') this.status = 'BLOCK_END'
+            if ((this.status === 'BLOCK_VALUE') && inline_code[i] === '{' || inline_code[i] === '}' ) this.status = 'BLOCK_VARIABLE'
             code_status.set(inline_code[i] + ' | ' + i, this.status)
         }
 
-        console.log(code_status)
+        for (const i of code_status) {
+            const element = i[0].split('|').map(x => x.trim())
+            const char    = element[0]
+            const line    = element[1]
+            const stat    = i[1]
+
+            console.log(char, line, stat)
+        }
 
         return this.content
 
