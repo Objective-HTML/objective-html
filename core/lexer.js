a9fc75305975cc425e2421af03766523e915ae9c
NessMC <contact@nessmc.fr>
2020-05-08 16:03:26 +0200

    ✨ Lexer variable added.

--- core/lexer.js
+++ core/lexer.js
@@ -23,7 +23,7 @@ export default class Lexer {
 
         const inline_code = this.content.split(/\r?\n/g).map(x => x.trim()).filter(x => x !== '').join('')
         const code_status = new Map()
-        
+
         for (const i in inline_code) {
             if (this.status === 'BLOCK_END') this.status = 'BLOCK_VALUE'
             if (this.status === 'BLOCK_START') this.status = 'BLOCK_CONTENT'
@@ -35,6 +35,11 @@ export default class Lexer {
             if (this.status === 'BLOCK_CONTENT') {
                 if (inline_code[i] === '>') {
                     this.status = 'BLOCK_END'
+                }
+            }
+            if (this.status === 'BLOCK_VALUE') {
+                if (inline_code[i] === '{' || inline_code[i] === '}' ) {
+                    this.status = 'BLOCK_VARIABLE'
                 }
             }
             code_status.set(inline_code[i] + ' | ' + i, this.status)
