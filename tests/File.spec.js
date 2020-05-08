b6515e19e7b9f03ef9e6d87ebde91764126fc5db
NessMC <contact@nessmc.fr>
2020-05-08 12:18:32 +0200

    🚚 Moved html files to html folder.

--- tests/File.spec.js
+++ tests/File.spec.js
@@ -4,19 +4,19 @@ import File from '../core/file'
 describe('#File', () => {
 
   it('should return file content', () => {
-    new File('tests/sum.html').read().then(content => {
+    new File('tests/html/sum.html').read().then(content => {
       Assert.notEqual(content, undefined)
     }) 
   })
 
   it('should throw an error if file does not exist', () => {
-    new File('tests/index.html').read().catch(error => {
+    new File('tests/html/index.html').read().catch(error => {
       Assert.notEqual(error, undefined)
     })
   })
 
   it('should return default value if file is empty', () => {
-    new File('tests/empty.html').read().then(content => {
+    new File('tests/html/empty.html').read().then(content => {
       Assert.equal(content, '')
     })
   })
