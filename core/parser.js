30796e233fb0ee7d27038fb1ae6f483c8052dd8e
NessMC <contact@nessmc.fr>
2020-05-09 13:08:43 +0200

    🔨 Parser restructuring system added.

--- core/parser.js
+++ core/parser.js
@@ -17,8 +17,8 @@ export default class Parser {
 
         if (typeof this.lexer !== 'object') throw new Error('Lexer content must be an object!')
 
-        let   blck_index = 0,
-              blocks     = []
+        let   blck_index  = 0,
+              blocks      = []
 
         for (const i of this.lexer) {
             const element    = i[0].split('|').map(x => x.trim()).map(x => x === '' ? x = ' ' : x),
@@ -26,37 +26,61 @@ export default class Parser {
                   content    = i[1]
             switch (content) {
                 case 'BLOCK_START': {
-
                     blocks.push([])
-                    blocks[blck_index][0] = char
+                    blocks[blck_index].push(char)
                     this.status = 'BLOCK_START'
                     break
                 }
 
                 case 'BLOCK_CONTENT': {
-
                     if (this.status === 'BLOCK_START' || this.status === 'BLOCK_CONTENT') {
-                        console.log('test')
                         blocks[blck_index].push(char)
                         this.status = 'BLOCK_CONTENT'
                     }
                     break
+                }
+                
+                case 'BLOCK_VALUE': {
+                    if (this.status === 'BLOCK_VALUE') {
+                        blocks[blck_index - 1].push(char)
+                        this.status = 'BLOCK_VALUE'
 
+                    } else if (this.status === 'BLOCK_END') {
+                        ++blck_index
+                        blocks.push([])
+                        blocks[blck_index - 1] = []
+                        blocks[blck_index - 1].push(char)
+                        this.status = 'BLOCK_VALUE'
+                    }
+                    break
+                }
+                
+                case 'BLOCK_VARIABLE': {
+                    if (this.status === 'BLOCK_VARIABLE') {
+                        blocks[blck_index - 1].push(char)
+                        this.status = 'BLOCK_VARIABLE'
+
+                    } else if (this.status === 'BLOCK_END') {
+                        ++blck_index
+                        blocks.push([])
+                        blocks[blck_index - 1] = []
+                        blocks[blck_index - 1].push(char)
+                        this.status = 'BLOCK_VARIABLE'
+                    }
+                    break
                 }
 
                 case 'BLOCK_END': {
-
                     if (this.status === 'BLOCK_CONTENT' || this.status === 'BLOCK_START') {
                         blocks[blck_index].push(char)
                         this.status = 'BLOCK_END'
                         ++blck_index
                     }
                     break
-
                 }
             }
         }
-        
+
         console.log(blocks.map(x => x.join('')))
 
     }
