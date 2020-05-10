ec1781f795b9bf1b92195168ac12296a18eeb1e3
NessMC <contact@nessmc.fr>
2020-05-10 12:43:19 +0200

    🔨 Basic transpiling operations added.

--- core/transpiler.js
+++ core/transpiler.js
@@ -10,5 +10,86 @@ export default class Transpiler {
   constructor (content = '') {
     this.parser = new Parser(content).parse()
   }
-  
+
+  transpile () {
+
+    const code = []
+
+    for (const item of this.parser) {
+      const block  = item.block,
+            type   = item.type,
+            id     = item.id,
+            args   = item.args,
+            params = item.parameters,
+            status = type.toLowerCase()
+                         .split('_')[1]
+      console.log(type, block)
+      switch (type) {
+
+        case 'COMMENT': {
+
+          code.push(block.replace('<!--', '/*')
+                         .replace('-->', '*/'))
+          break
+          
+        }
+
+        case 'DEFINE_START' : {
+
+          code.push('var ' + item.parameters[0].value + '=')
+          break
+
+        }
+
+        case 'VARIABLE': {
+          
+          code.push(block.replace(/(\{|\})/g, ''))
+          break
+          
+        }
+
+        case 'TEXT': {
+          code.push('"' + block + '"')
+          break
+        }
+
+        case 'DEFINE_END': {
+
+          code.push(';')
+
+        }
+        
+        case 'FUNCTION_START': {
+          let function_args = ''
+          let function_name = ''
+          for (const i of params) {
+            if (i.name) {
+              function_name = i.value
+            }
+          }
+          if (args) {
+            function_args = args.join(',')
+          }
+          code.push('function ' + function_name + '(' + function_args + ') {')
+          break
+        }
+
+        case 'FUNCTION_END': {
+          code.push('}')
+          break
+        }
+
+        case 'OBJECTIVE_START': {
+          console.log(id)
+          break
+        }
+
+      }
+      
+    }
+    
+    console.log(code)
+
+  }
+
 }
--- html.js
+++ html.js
@@ -3,9 +3,9 @@
               Main
 //////////////////////////////*/
 
-import Parser from './core/parser'
-import File   from './core/file'
+import Transpiler from './core/transpiler'
+import File       from './core/file'
 
 new File('tests/html/sum.html').read().then(content => {
-     console.log(new Parser(content).parse())
+     console.log(new Transpiler(content).transpile())
 })
