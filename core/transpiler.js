340598c3cd54d1644884af8da0fdd1389ad29819
NessMC <contact@nessmc.fr>
2020-05-10 12:04:26 +0200

    🚧 Transpiler WIP.

--- core/transpiler.js
+++ core/transpiler.js
@@ -2,3 +2,13 @@
          OBJECTIVE HTML
            Transpiler
 //////////////////////////////*/
+
+import Parser from './parser'
+
+export default class Transpiler {
+
+  constructor (content = '') {
+    this.parser = new Parser(content).parse()
+  }
+  
+}
