92eaeda0839cb1f824e301fdeee8ef2cd34a1b80
NessMC <contact@nessmc.fr>
2020-05-13 14:53:29 +0200

    ✨ Recursive module reading

--- html.js
+++ html.js
@@ -4,53 +4,24 @@
 //////////////////////////////*/
 
 import Transpiler from './core/transpiler'
-import File       from './core/file'
 import FS         from 'fs'
+import path       from 'path'
+import Parser     from './core/parser'
 
-export default class Objective {
-
-     constructor (input) {
-
-          this.input = input
-
-     }
- 
-     transpile () {
-          return new Promise ((resolve, reject) => {
-               FS.stat(this.input, (error, status) => {
-                    if (error) throw error
-                    if (status.isDirectory()) {
-                         const code = new Map()
-                         new File(this.input).filewalker((error, files) => {
-                              if (error) throw error 
-                              for (const i of files) {
-                                   if (i.endsWith('.html')) {
-                                        FS.readFile(i, 'UTF-8', (error, content) => {
-                                             if (error) throw error
-                                             code.set(i, new Transpiler(content).transpile())
-                                             if (code.size === files.length) {
-                                                  resolve(code)
-                                             }
-                                        })
-                                   }
-                              }
-                         })
-                    } else {
-                         FS.readFile(this.input, 'UTF-8', (error, content) => {
-                              if (error) throw error
-                              resolve(new Transpiler(content).transpile())
-                         })
+function readFile (file) {
+     FS.readFile(path.resolve(path.join(__dirname, file)), 'UTF-8', (error, content) => {
+          if (error) throw error
+          console.log(new Transpiler(content).transpile())
+          for (const item of new Parser(content).parse()) {
+               if (item.type.endsWith('_START') && item.block === 'import') {
+                    for (const param of item.parameters) {
+                         if (param.name === 'src') {
+                              readFile(path.dirname(file) + '/' + param.value + '.html')
+                         }
                     }
-               })
-
-          })
-
-     }
-
+               }
+          }
+     })
 }
 
-import PATH from 'path'
-const test = new Objective(PATH.resolve(PATH.join(__dirname, 'tests/html')))
-
-test.transpile().then(content => console.log(content))
-
+readFile('tests/html/math.html')
