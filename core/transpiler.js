fd3a4143180b99f7ab1cb07415b83dd2654a0d80
NessMC <contact@nessmc.fr>
2020-05-13 17:35:03 +0200

    🔨 Module import and export transpiling added.

--- core/transpiler.js
+++ core/transpiler.js
@@ -5,8 +5,6 @@
 
 import Parser     from './parser'
 import Conditions from './tokens/conditions'
-import fs         from 'fs'
-import path       from 'path'
 
 export default class Transpiler {
 
@@ -20,23 +18,58 @@ export default class Transpiler {
     
     const code = []
     for (const i of this.parser) {
-      const block  = i.block      || new String(''),
-            id     = i.id         || new Number(0),
-            type   = i.type       || new String(''),
-            args   = i.type       || new Array(),
-            params = i.parameters || new Array(),
-            all    = i.all        || new Array()
+      const block       = i.block      || new String(''),
+            id          = i.id         || new Number(0),
+            type        = i.type       || new String(''),
+            args        = i.type       || new Array(),
+            params      = i.parameters || new Array(),
+            all         = i.all        || new Array()
+      let   export_stat = false
       if (type.endsWith('_START')) {
+        
         if (block === 'import') {
+          let SRC  = undefined,
+              AS   = undefined,
+              NAME = undefined
 
-          code.push(block, all)
+          if (params.length < 1) return new Error('test')
+
+          for (const param of params) {
+            if (param.name === 'src')       SRC  = param.value
+            else if (param.name === 'as')   AS   = param.value
+            else if (param.name === 'name') NAME = param.value
+          }
+
+          if (SRC) {
+            if (AS) {
+              if      (AS === 'JS')      SRC  = SRC.replace('./', '') 
+              else if (AS === 'package') SRC  = './modules/' + SRC.replace('./', '')
+            } else                       SRC  = './' + SRC + '.html'
+            if        (!NAME)            NAME = SRC.split('/').pop().replace('.html', '')
+            code.push(`const ${NAME}=require('${SRC.replace('.html', '.js')}')`)
+          }
+
+        } else if (block === 'export') {
+
+          code.push(`module.exports={`)
+          export_stat = true
 
         }
+      } else if (type.endsWith('_END')) {
+
+        if (block === 'import') {
+          code.push(';')
+        }
+
+        if (block === 'export') {
+          code.push('};')
+          export_stat = false
+        }
+
       }
     }
 
-    return code
-
+    return code.join('')
   }
 
 }
