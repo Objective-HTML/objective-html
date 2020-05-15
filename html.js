b41238c070fd03f6fa75ffaab3e28c507dd78f24
NessMC <contact@nessmc.fr>
2020-05-15 11:49:13 +0200

    🔨 Variable and text parsing updated.

--- html.js
+++ html.js
@@ -4,32 +4,45 @@
 //////////////////////////////*/
 
 import Transpiler from './core/transpiler'
+import Parser     from './core/parser'
 import FS         from 'fs'
 import path       from 'path'
-import Parser     from './core/parser'
-import Lexer      from './core/lexer'
 const test = []
 
-console.log(new Parser(FS.readFileSync('./tests/html/math.html', 'UTF-8')).parse())
+// console.log(new Transpiler(FS.readFileSync('tests/html/math.html', 'UTF-8')).transpile())
 
-// function readFile (file) {
-//      const content = FS.readFileSync(path.resolve(path.join(__dirname, file)), 'UTF-8')
-//      test.push(file)
-//      for (const item of new Parser(content).parse()) {
-//           if (item.type.endsWith('_START') && item.block === 'import') {
-//                for (const param of item.parameters) {
-//                     if (param.name === 'src') {
-//                          readFile(path.dirname(file) + '/' + param.value + '.html')
-//                     }
-//                }
-//           }
-//      }
-// }
+function readFile (file) {
+     const content = FS.readFileSync(path.resolve(path.join(__dirname, file)), 'UTF-8')
+     test.push(file)
+     for (const item of new Parser(content).parse()) {
+          if (item.type === 'START' && item.block === 'import') {
+               for (const param of item.params) {
+                    if (param.name === 'src') {
+                         readFile(path.dirname(file) + '/' + param.value + '.html')
+                    }
+               }
+          }
+     }
+}
 
-// readFile('tests/html/math.html')
+readFile('tests/html/math.html')
+console.log(test)
+const files = new Transpiler(test.reverse()).transpile()
 
-// const files = new Transpiler(test.reverse()).transpile()
+for (const file of files) {
+     FS.writeFileSync(path.resolve(path.join(file[0].replace('.html', '.js'))), file[1])
+}
 
-// for (const file of files) {
-//      FS.writeFileSync(path.resolve(path.join(file[0].replace('.html', '.js'))), file[1])
-// }
+
+var util = require('util'),
+    vm = require('vm')
+
+let helloScript = new vm.Script(';', {
+     produceCachedData: true /* This is required for Node.js < 10.0.0 */
+});
+
+
+// or in Node.js >= 10
+let helloBuffer = helloScript.createCachedData();
+
+helloScript.runInThisContext();
