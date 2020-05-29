d4adb505bb3ff77b7c225f7a0b29f1304d99850b
NessMC <contact@nessmc.fr>
2020-05-29 15:54:52 +0200

    ✨ Event emitter added.

--- core/events.js
+++ core/events.js
@@ -2,3 +2,27 @@
          OBJECTIVE HTML
              Events
 //////////////////////////////*/
+
+export default class ObjectiveEmitter {
+
+    constructor () {
+        this.handlers = {}
+    }
+
+    on (event, handler) {
+        this.handlers[event] = this.handlers[event] || []
+        let hdl = this.handlers[event]
+        hdl.push(handler)
+        return this
+    }
+
+    emit (event, ...args) {
+        this.handlers[event] = this.handlers[event] || []
+        let hdl = this.handlers[event]
+        hdl.forEach(it => {
+            it.apply(this, args)
+        })
+        return this
+    }
+
+}
