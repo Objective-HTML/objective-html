64d4ed1e2bc7ee5f50a7074652e4b2a57d9cb915
NessMC <contact@nessmc.fr>
2020-05-29 16:03:13 +0200

    🔨 Addon creation class added.

--- core/addon.js
+++ core/addon.js
@@ -2,3 +2,47 @@
          OBJECTIVE HTML
              Addons
 //////////////////////////////*/
+
+import ObjectiveHTML from './parser'
+
+export default class ObjectiveAddon extends ObjectiveHTML {
+
+    constructor (content) {
+        
+        super(content)
+        
+    }
+
+    create (object = {
+        tagName : String,
+        onOpen  : Function,
+        onClose : Function,
+        onText  : Function
+    }) {
+
+        if (typeof object !== 'object') return
+
+        if (object.onClose) {
+            if (typeof object.onClose === 'function') {
+                this.on('close', (data, index) => {
+                    if (data.startsWith('</' + object.tagName)) {
+                        object.onClose(data, index)
+                    }
+                })
+            }
+        }
+        if (object.onOpen) {
+            if (typeof object.onOpen === 'function') {
+                this.on('open', (data, index) => {
+                    if (data.startsWith('<' + object.tagName)) {
+                        const attributes = parse.parseFragment(data).childNodes[0].attrs
+                        if (!attributes) attributes = []
+                        object.onOpen(data, index, attributes)
+                    }
+                })
+            }
+        }
+
+    }
+
+}
