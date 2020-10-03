9f027acfb38eacbd4513132195b852ff491f8d55
NessMC <contact@nessmc.fr>
2020-10-03 19:21:39 +0200

    ✅ Parser test added.

---
+++ tests/parser.test.ts
@@ -0,0 +1,13 @@
+import fs from 'fs/promises';
+import Parser from 'core/parser';
+
+describe('#Parser', () => {
+  describe('#Raw parsing', () => {
+    test('should return correct raw AST.', async () => {
+      const code: string = await fs.readFile('./sample/index.html', 'utf-8');
+      const expectedCode: string = await fs.readFile('./tests/content/example.ast.json', 'utf-8');
+      const parser: Parser = new Parser(code);
+      expect(parser.rawAST()).toEqual(JSON.parse(expectedCode));
+    });
+  });
+});
