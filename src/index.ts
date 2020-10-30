a517e2202622552ae962dc823503772c963671e6
thomasvergne <thomas@quark-lang.dev>
2020-10-30 19:12:18 +0100

    ✅ Parser HTML file updated.

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,6 @@ async function main() {
     const content: string = await fs.readFile('./sample/index.html', 'utf-8');
     const compiler: Compiler = new Compiler(content);
     console.log(await compiler.compile());
-    console.log(JSON.stringify(compiler.astRaw, null, 2));
   } catch (exception) {
     throw new Error(exception);
   }
